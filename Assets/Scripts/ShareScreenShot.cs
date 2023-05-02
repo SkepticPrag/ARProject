3b992a0d65e8f21f182f96bf64f6d7e03573c805
Germán Lobbia <germanlobbia@gmail.com>
2023-05-02 20:59:55 -0300

    [A] Screenshot & Share

--- Assets/Scripts/ShareScreenShot.cs
+++ Assets/Scripts/ShareScreenShot.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ShareScreenShot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _mainMenuCanvas;
+    private ARPointCloudManager _aRPointCloudManager;
+
+    private void Start()
     {
-        
+        _aRPointCloudManager = FindObjectOfType<ARPointCloudManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeScreenshot()
     {
-        
+        TurnOffOnARContents();
+        StartCoroutine(TakeScreenshotAndShare());
+    }
+
+    private void TurnOffOnARContents()
+    {
+        var points = _aRPointCloudManager.trackables;
+        foreach (var point in points)
+        {
+            point.gameObject.SetActive(!point.gameObject.activeSelf);
+
+        }
+        _mainMenuCanvas.SetActive(!_mainMenuCanvas.activeSelf);
+    }
+
+    private IEnumerator TakeScreenshotAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+        // To avoid memory leaks
+        Destroy(ss);
+
+        new NativeShare().AddFile(filePath)
+            .SetSubject("Subject goes here").SetText("Look! This is the AR project!")
+            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .Share();
+        TurnOffOnARContents();
+        // Share on WhatsApp only, if installed (Android only)
+        //if( NativeShare.TargetExists( "com.whatsapp" ) )
+        //	new NativeShare().AddFile( filePath ).AddTarget( "com.whatsapp" ).Share();
     }
 }
